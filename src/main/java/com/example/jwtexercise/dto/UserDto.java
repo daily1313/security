14dcfb3765723f2cb748f66a1c8c3d3c6018ed44
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-05 20:03:40 +0900

    feat : User API

---
+++ src/main/java/com/example/jwtexercise/dto/UserDto.java
@@ -0,0 +1,49 @@
+package com.example.jwtexercise.dto;
+
+import com.example.jwtexercise.entity.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+
+    private Set<AuthorityDto> authorityDtoSet;
+
+    public static UserDto from(User user) {
+        if(user == null) return null;
+
+        return UserDto.builder()
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .authorityDtoSet(user.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+
+
+}
